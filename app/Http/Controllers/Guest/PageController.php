551a153a705b583ea6030cd4de9cbc006d66e8df
GianluGiarde <gianlugiarde@gmail.com>
2023-06-26 18:12:51 +0200

    add controller links

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function movies()
+    {
+        $collMovies = Movie::all();
+
+        return view('movies', compact('collMovies'));
+    }
+}
