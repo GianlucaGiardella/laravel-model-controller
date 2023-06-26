59e3ac0cfecc0d5d64822eadc46e09b27dfb4a89
GianluGiarde <gianlugiarde@gmail.com>
2023-06-27 00:12:31 +0200

    add movie info & bootstrap style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -2,10 +2,21 @@
 
 
 @section('content')
-    <h1>Movies</h1>
-    <ul>
+    <h1 class="text-center">Movies</h1>
+    <hr>
+    <ul class="d-flex flex-wrap row row-cols-4 g-4">
         @foreach ($collMovies as $movie)
-        <li>{{ $movie->title }}</li>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="" class="card-img-top" alt="{{ $movie->name }}">
+                <div class="card-body">
+                <h5 class="card-title">{{ $movie->name }}</h5>
+                <p class="card-text">Nazionalità: {{ $movie->nationality }}</p>
+                <p class="card-text">Data di uscita: {{ $movie->date }}</p>
+                <p class="card-text">Voto: {{ $movie->vote }}</p>
+                </div>
+            </div>
+        </div>
         @endforeach
     </ul>
 @endsection
