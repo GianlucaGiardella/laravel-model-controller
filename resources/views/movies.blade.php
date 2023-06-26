80a3af7f38d640bea70d646b916049ff936e4ea6
GianluGiarde <gianlugiarde@gmail.com>
2023-06-27 00:51:25 +0200

    fix style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
     <h1 class="text-center text-light mb-4">Movies</h1>
-    <ul class="d-flex flex-wrap row row-cols-4 g-4">
+    <div class="row row-cols-4">
         @foreach ($collMovies as $movie)
-        <div class="col">
-            <div class="card" style="width: 18rem;">
+        <div class="col g-4">
+            <div class="card w-100" style="width: 18rem;">
                 <div class="card-body bg-secondary-subtle">
                     <h5 class="card-title">{{ $movie->name }}</h5>
                     <p class="card-text">Nazionalità: {{ $movie->nationality }}</p>
@@ -16,5 +16,5 @@
             </div>
         </div>
         @endforeach
-    </ul>
+    </div>
 @endsection
